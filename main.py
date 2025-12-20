e59be5d20af64278dbe9735add5aaef6caf4a0df
MithilManchukonda <mithilmanchukonda@gmail.com>
2025-12-20 21:01:16 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,30 @@
+import requests
+from send_email import send_email
+
+topic = "tesla"
+
+api_key = "cc120f93bdd54312b8cd04327be63cd1"
+
+url = (
+    "https://newsapi.org/v2/everything?"
+    "q=tesla&"
+    "from=2025-11-16&"
+    "sortBy=publishedAt&"
+    f"apiKey={api_key}&"
+    "language=en"
+)
+
+request = requests.get(url)
+content = request.json()
+
+body = "Subject: Tesla News Updates\n\n"
+
+for article in content["articles"][:20]:
+    if article["title"]:
+        body += (
+            article["title"] + "\n"
+            + (article["description"] or "") + "\n"
+            + article["url"] + "\n\n"
+        )
+
+send_email(body.encode("utf-8"))
